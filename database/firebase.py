f6800b2862a358471f581d8828fbd4c455b0e255
perla subba venkata keerthi <keerthiperla2006@gmail.com>
2026-07-21 14:49:06 +0530

    Update firebase configuration for Render

--- database/firebase.py
+++ database/firebase.py
@@ -1,7 +1,16 @@
+import os
+import json
+import tempfile
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase_key.json")
+firebase_data = json.loads(os.environ["FIREBASE_KEY"])
+
+with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".json") as temp:
+    json.dump(firebase_data, temp)
+    temp.flush()
+
+    cred = credentials.Certificate(temp.name)
 
 firebase_admin.initialize_app(cred)
 
